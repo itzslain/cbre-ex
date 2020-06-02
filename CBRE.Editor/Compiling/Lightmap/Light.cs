89101d52f64930410271da86be54f36f09d5a496
juanjp600 <juanjprot@gmail.com>
2020-06-02 18:55:17 -0300

    Made Light property parsing more robust

--- CBRE.Editor/Compiling/Lightmap/Light.cs
+++ CBRE.Editor/Compiling/Lightmap/Light.cs
@@ -25,30 +25,53 @@ namespace CBRE.Editor.Compiling.Lightmap
         {
             lightEntities = new List<Light>();
             lightEntities.AddRange(map.WorldSpawn.Find(q => q.ClassName == "light").OfType<Entity>()
-                .Select(x => new Light()
+                .Select(x =>
                 {
-                    Origin = new CoordinateF(x.Origin),
-                    Range = float.Parse(x.EntityData.GetPropertyValue("range"), CultureInfo.InvariantCulture),
-                    Color = new CoordinateF(x.EntityData.GetPropertyCoordinate("color")),
-                    Intensity = float.Parse(x.EntityData.GetPropertyValue("intensity"), CultureInfo.InvariantCulture),
-                    HasSprite = bool.Parse(x.EntityData.GetPropertyValue("hassprite") ?? "true"),
-                    Direction = null,
-                    innerCos = null,
-                    outerCos = null
+                    float range = 100.0f;
+                    float.TryParse(x.EntityData.GetPropertyValue("range"), NumberStyles.Float, CultureInfo.InvariantCulture, out range);
+                    float intensity = 1.0f;
+                    float.TryParse(x.EntityData.GetPropertyValue("intensity"), NumberStyles.Float, CultureInfo.InvariantCulture, out intensity);
+                    bool hasSprite = true;
+                    bool.TryParse(x.EntityData.GetPropertyValue("hassprite") ?? "true", out hasSprite);
+
+                    return new Light()
+                    {
+                        Origin = new CoordinateF(x.Origin),
+                        Range = range,
+                        Color = new CoordinateF(x.EntityData.GetPropertyCoordinate("color")),
+                        Intensity = intensity,
+                        HasSprite = hasSprite,
+                        Direction = null,
+                        innerCos = null,
+                        outerCos = null
+                    };
                 }));
             lightEntities.AddRange(map.WorldSpawn.Find(q => q.ClassName == "spotlight").OfType<Entity>()
                 .Select(x =>
                 {
+                    float range = 100.0f;
+                    float.TryParse(x.EntityData.GetPropertyValue("range"), NumberStyles.Float, CultureInfo.InvariantCulture, out range);
+                    float intensity = 1.0f;
+                    float.TryParse(x.EntityData.GetPropertyValue("intensity"), NumberStyles.Float, CultureInfo.InvariantCulture, out intensity);
+                    bool hasSprite = true;
+                    bool.TryParse(x.EntityData.GetPropertyValue("hassprite") ?? "true", out hasSprite);
+                    float innerCos = 0.5f;
+                    float.TryParse(x.EntityData.GetPropertyValue("innerconeangle"), NumberStyles.Float, CultureInfo.InvariantCulture, out innerCos);
+                    innerCos = (float)Math.Cos(innerCos*(float)Math.PI / 180.0f);
+                    float outerCos = 0.75f;
+                    float.TryParse(x.EntityData.GetPropertyValue("outerconeangle"), NumberStyles.Float, CultureInfo.InvariantCulture, out outerCos);
+                    outerCos = (float)Math.Cos(outerCos * (float)Math.PI / 180.0f);
+
                     Light light = new Light()
                     {
                         Origin = new CoordinateF(x.Origin),
-                        Range = float.Parse(x.EntityData.GetPropertyValue("range"), CultureInfo.InvariantCulture),
+                        Range = range,
                         Color = new CoordinateF(x.EntityData.GetPropertyCoordinate("color")),
-                        Intensity = float.Parse(x.EntityData.GetPropertyValue("intensity"), CultureInfo.InvariantCulture),
-                        HasSprite = bool.Parse(x.EntityData.GetPropertyValue("hassprite") ?? "true"),
+                        Intensity = intensity,
+                        HasSprite = hasSprite,
                         Direction = null,
-                        innerCos = (float)Math.Cos(float.Parse(x.EntityData.GetPropertyValue("innerconeangle"), CultureInfo.InvariantCulture) * (float)Math.PI / 180.0f),
-                        outerCos = (float)Math.Cos(float.Parse(x.EntityData.GetPropertyValue("outerconeangle"), CultureInfo.InvariantCulture) * (float)Math.PI / 180.0f)
+                        innerCos = innerCos,
+                        outerCos = outerCos
                     };
 
                     Coordinate angles = x.EntityData.GetPropertyCoordinate("angles");
