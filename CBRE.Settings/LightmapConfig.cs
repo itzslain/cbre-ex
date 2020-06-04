77d11bc62550272de0e82bf0edd00a1e9fb34aef
juanjp600 <juanjprot@gmail.com>
2020-06-04 16:37:19 -0300

    Changed version number + changed default lightmapper parameters

--- CBRE.Settings/LightmapConfig.cs
+++ CBRE.Settings/LightmapConfig.cs
@@ -23,9 +23,9 @@ namespace CBRE.Settings
 
         static LightmapConfig()
         {
-            DownscaleFactor = 10;
+            DownscaleFactor = 15;
             PlaneMargin = 1;
-            TextureDims = 2048;
+            TextureDims = 512;
             BlurRadius = 2;
 
             AmbientColorR = 30;
