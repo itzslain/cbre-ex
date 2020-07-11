49db672ac39b762a78ae79952753816ebe592fda
juanjp600 <juanjprot@gmail.com>
2020-07-11 19:03:11 -0300

    Minor refactor

---
+++ CBRE.Editor/Compiling/ExportWriterExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace CBRE.Editor.Compiling
+{
+    public static class ExportWriterExtensions
+    {
+        public static void WriteByteString(this BinaryWriter writer, string str)
+        {
+            writer.Write((byte)str.Length);
+            for (int i = 0; i < str.Length; i++)
+            {
+                writer.Write((byte)str[i]);
+            }
+        }
+
+        public static void WriteB3DString(this BinaryWriter writer, string str)
+        {
+            writer.Write((UInt32)str.Length);
+            for (int i = 0; i < str.Length; i++)
+            {
+                writer.Write((byte)str[i]);
+            }
+        }
+    }
+}
