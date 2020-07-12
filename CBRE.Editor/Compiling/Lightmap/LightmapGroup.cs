85e3b6b4ac4c10a8ae6a75407d383277df213fa6
Saalvage <saalvage@gmail.com>
2020-07-12 17:57:06 +0200

    Removed debug code

--- CBRE.Editor/Compiling/Lightmap/LightmapGroup.cs
+++ CBRE.Editor/Compiling/Lightmap/LightmapGroup.cs
@@ -19,18 +19,8 @@ namespace CBRE.Editor.Compiling.Lightmap {
         public int writeX;
         public int writeY;
 
-#if DEBUG
-        public bool DebugBreakpoint = false;
-#endif
-
         private void CalculateInitialUV() {
             if (uAxis == null || vAxis == null) {
-#if DEBUG
-                if (DebugBreakpoint) {
-                    int breakHerePls = 2222222;
-                }
-#endif
-
                 var direction = Plane.GetClosestAxisToNormal();
                 var tempV = direction == CoordinateF.UnitZ ? -CoordinateF.UnitY : -CoordinateF.UnitZ;
                 uAxis = Plane.Normal.Cross(tempV).Normalise();
