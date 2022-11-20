7a35c1111382b9dc263e471d661261c3f1383a22
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2022-11-20 03:10:44 +0100

    Fix log in export form not resizing.

--- Source/CBRE.Editor/Compiling/ExportForm.Designer.cs
+++ Source/CBRE.Editor/Compiling/ExportForm.Designer.cs
@@ -233,6 +233,7 @@
 	        // 
 	        // panel1
 	        // 
+	        this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
 	        this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
 	        this.panel1.Controls.Add(this.ProgressLog);
 	        this.panel1.Location = new System.Drawing.Point(12, 126);
