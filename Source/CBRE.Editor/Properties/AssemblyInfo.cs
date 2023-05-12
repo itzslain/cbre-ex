b61899fb996d3d53ef5e029958d271909176dddf
AnalogFeelings <51166756+AnalogFeelings@users.noreply.github.com>
2023-05-12 19:17:48 +0200

    Bump version to 2.2.

--- Source/CBRE.Editor/Properties/AssemblyInfo.cs
+++ Source/CBRE.Editor/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CBRE-EX")]
-[assembly: AssemblyCopyright("Copyright © AestheticalZ 2022, based on CBRE by juanjp600, which in turn is based on Sledge by http://logic-and-trick.com 2014")]
+[assembly: AssemblyCopyright("Copyright © Analog Feelings 2022, based on CBRE by juanjp600, which in turn is based on Sledge by http://logic-and-trick.com 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
